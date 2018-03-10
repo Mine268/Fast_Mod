d1f61df2e5cf50be3ea61f254a086a047c77c6b8
Mine268 <mine268@foxmail.com>
2018-03-10 20:22:33 +0800

    add

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int powMod(long long base, long n, long c) {
+	long long ans = 1, b = n;
+	base = base % c;
+	while(b != 0) {
+		if(b & 1 == 1)
+			ans = ans * base % c;
+		base = base * base % c;
+		b >>= 1;
+	}
+	return ans;
+}
+
+int main() {
+	long long base, n, c;
+	scanf("%lld %lld %lld", &base, &n, &c);
+	long long ans = powMod(base, n, c);
+	printf("%d", ans);
+	return 0;
+}
